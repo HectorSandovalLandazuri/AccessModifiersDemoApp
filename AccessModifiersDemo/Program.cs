3c2b4a78019fd10e4e44ed7b1ff377fb1d39fbe2
Héctor Manuel Sandoval Landázuri <ingsan@live.com.mx>
2023-01-13 20:06:41 -0600

    Agregar archivos de proyecto.

---
+++ AccessModifiersDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DemoLibrary;
+
+Person person = new Person();
+
+person.SSN = "111-11-1111";
+person.SayHello();
+
+public class ModifiedDataAccess : DataAccess
+{
+    public void GetUnsecureConnectionInfo()
+    {
+        GetConnectionString();
+    }
+}
+
+public class CEO : Manager
+{
+    public void GetConnectionInfo()
+    {
+        ModifiedDataAccess data = new ModifiedDataAccess();
+        data.GetUnsecureConnectionInfo();
+        
+    }
+}
